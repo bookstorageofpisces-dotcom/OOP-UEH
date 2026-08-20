486c2156a08a13c740b7e343968f3cda61610f1c
Pisces's Storage <bookstorageofpisces@gmail.com>
2026-08-20 16:41:08 +0700

    BTVN week2-EX1

---
+++ Week2/OOPWeek2/EX1/BTVN/BTVN/Program.cs
@@ -0,0 +1,34 @@
+﻿using BTVN;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Student[] ListStudents = new Student[5];
+        ListStudents[0] = new Student("Nguyen Van A", 9);
+        ListStudents[1] = new Student("Tran Thi B", 8);
+        ListStudents[2] = new Student("Le Van C", 7);
+        ListStudents[3] = new Student("Pham Thi D", 6);
+        ListStudents[4] = new Student("Hoang Van E", 5);
+        Console.WriteLine("Tổng số học sinh:" +Student.GetTotalStudent());
+        ListStudents[0].GetTotalStudent();
+        foreach (Student student in ListStudents)
+        {
+            Console.WriteLine("Tên học sinh: " + student.GetName());
+            Console.WriteLine("Điểm số: " + student.GetScore());
+            Console.WriteLine("Xếp loại: " + student.GetClassification());
+            if (student.IsPassed())
+            {
+                Console.WriteLine("Kết quả: Đậu");
+            }
+            else
+            {
+                Console.WriteLine("Kết quả: Rớt");
+            }
+            Console.WriteLine("-----------------------------");
+        }
+        Console.WriteLine("Học sinh xuất sắc nhất:"+Student.FindTopStudent(ListStudents));
+        Console.WriteLine("Điểm trung bình cả lớp:" + Student.CalculateAverageScore(ListStudents));
+    }
+}
---
+++ Week2/OOPWeek2/EX1/BTVN/BTVN/Student.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BTVN
+{
+    public class Student
+    {
+        private string name;
+        private double score;
+        private static int totalStudents = 0;
+
+        public Student(string name, double score)
+        {
+            this.name = name;
+            this.score = score;
+            totalStudents++;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public double GetScore()
+        {
+            return score;
+        }
+        public bool IsPassed()
+        {
+           if (score >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string GetClassification()
+        {
+            if (score >= 8)
+            {
+                return "Excellent";
+            }
+            else if (score >= 6.5)
+            {
+                return "Good";
+            }
+            else if (score >= 5)
+            {
+                return "Average";
+            }
+            else
+            {
+                return "Weak";
+            }
+        }
+        public static int GetTotalStudent()
+        {
+            return totalStudents;
+        }
+        public static string FindTopStudent(Student[] students)
+        {
+            if (students == null || students.Length == 0)
+            {
+                return null;
+            }
+            Student top = students[0];
+            foreach (Student student in students)
+            {
+                if (student.score > top.score)
+                {
+                    top = student;
+                }
+            }
+            return top.name;
+        }
+        public static double CalculateAverageScore(Student[] students)
+        {             
+            if (students == null || students.Length == 0)
+            {
+                return 0;
+            }
+            double totalScore = 0;
+            foreach (Student student in students)
+            {
+                totalScore += student.score;
+            }
+            return totalScore / students.Length;
+        }
+    }
+}
